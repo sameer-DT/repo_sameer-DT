c68b2a89fdd7bc3b7fef156f12d81bff9db26391
DT_sameer <sameer.shaik@dispatchtrack.com>
2024-06-15 21:18:14 +0530

    changes applied

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
-    def home
-    end
+        def render_404
+          render file: "#{Rails.root}/public/404.html", status: :not_found
+        end      
+
+        def render_500
+            render file:"#{Rails.root}/public/500.html",status: :not_found
+        end
 end
